08be6ac22bbc3495e98fe1f68a1be61dec675a96
dandamasek <masekdaniel02@gmail.com>
2021-06-13 13:07:41 +0200

    dd

--- try/try/Program.cs
+++ try/try/Program.cs
@@ -5,10 +5,41 @@ namespace try
 
 internal class Program
 {
+    static Random random = new Random();
     public static void Main(string[] args)
     {
-        Console.WriteLine("hdddoj");
+        int[] a = new int[10];
+        for (int i = 0; i < a.Lenght; i++)
+            a[i] = random.Next(a.Lenght);
+            Console.Write("{0}, ", a[i]);
+
+            Console.ReadLine();
     }
+
+    static void SelectionSort(int[] a)
+    {
+        for (int j = 0; j < a.lenght-1; j++)
+        {
+            int iMin = j;
+            for (int i = j+1; i < args.; i++)
+            {
+                if (a[i] < a[iMin])
+                    iMin = i;
+            }
+            Prohod(a, j, iMin);
+        }
+    }
+
+    static void Prohod(int[] a, int i1, int i2)
+    {
+        if (i1 != i2)
+        {
+            int p = a[i1];
+            a[i1] = a[i2];
+            a[i2] = p;
+        }
+    }
+    
 }
 
 }
