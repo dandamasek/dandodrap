1a223b8a7b9332b0e2655ed6ee1b14c6c89ad747
dandamasek <masekdaniel02@gmail.com>
2021-06-17 00:29:50 +0200

    d

---
+++ piskvorky/piskvorky/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace piskvorky
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("danda");
+        }
+    }
+}
